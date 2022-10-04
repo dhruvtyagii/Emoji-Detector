1b394ee6b61b51f4338cd6153a0c0a8588aaf64e
Dhruv Tyagi <52095796+dhruvtyagii@users.noreply.github.com>
2022-10-04 20:02:16 +0530

    React
    React

--- my/src/App.js
+++ my/src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, {useState} from "react";
+import "./styles.css"
+
+var welcome = 'Tanay';
+
 function App() {
+  const [likeCounter, setLikeCounter] = useState(0)
+  var  [userInput, setUserInput] = useState("")
+
+      function likeButtonClicked() {
+
+        var newLikeCounter = likeCounter + 1;
+        setLikeCounter(newLikeCounter);
+        console.log(likeCounter);
+
+      }
+
+      function inputChangeHandler(event) {
+        setUserInput(event.target.value);
+
+      }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Welcome <span style = {{color:'blue'}}> {welcome} </span></h1> {likeCounter}
+        <button onClick={likeButtonClicked}>Click Me</button>
+        <input onChange={inputChangeHandler}></input> {userInput}
     </div>
   );
 }
