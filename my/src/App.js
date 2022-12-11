c0216b5c76a24af75fb32df29c26c0b9e3bbe72c
Dhruv Tyagi <52095796+dhruvtyagii@users.noreply.github.com>
2022-12-11 13:35:22 +0530

    Update App.js

--- my/src/App.js
+++ my/src/App.js
@@ -35,24 +35,39 @@ function App() {
     console.log(userInput);
 
   }
+
+  function clickEmoji(event) {
+    var userInput = event.target.name;
+    meaning = userInput;
+
+    if (meaning === undefined){
+      meaning = "we don't have this in our database";
+    }
+    
+    setMeaning(meaning);
+
+    console.log(meaning);
+    console.log(userInput);
+
+  }
   
 
   return (
     <div className="App">
         <h1> Emoji Tracker </h1>
         <input onChange={inputChangeHandler}></input>
-
+  
         <div> {meaning} </div>
         <div> Our Database is : 
-                😻
-                😇
-                🥰
-                😈
-                😊
-                😸
-                😄
-                😙
-                🙂
+                <button name= "love" onClick={clickEmoji}>😻</button> 
+                <button name = "angelic" onClick={clickEmoji} >😇</button>
+                <button name = "blushing" onClick={clickEmoji}>🥰</button>
+                <button name = "devilish" onClick={clickEmoji}>😈</button>
+                <button name = "happy" onClick={clickEmoji}>😊</button>
+                <button name = "very happy" onClick={clickEmoji}>😸</button>
+                <button name = "laughing" onClick={clickEmoji}>😄</button>
+                <button name = "whistling" onClick={clickEmoji}>😙</button>
+                <button name = "smiling" onClick={clickEmoji}>🙂</button>
        </div>
     </div>
   );
